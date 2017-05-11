47f5e0dfb015ec87e56a6703d98a481f42626be2
Gitelzon Dmitri <dmitri@yotpo.com>
2017-05-11 14:02:06 +0300

    fix(ActiveSupport):except bug

--- lib/restash_rails/log_subscriber/subscribers/active_support.rb
+++ lib/restash_rails/log_subscriber/subscribers/active_support.rb
@@ -60,7 +60,7 @@ module RestashRails
             response_code: extract_status(payload),
             path: payload[:path].to_s,
             format: payload[:format].to_s,
-            request_params: payload[:params].except(*excepted_params),
+            request_params: (payload[:params].except(*excepted_params) rescue {}),
             duration: event.duration.to_f.round(2),
             redirect_to: payload[:location] || '',
             db_duration: ((payload[:db_runtime] * 100).round(2)/100.0 rescue 0.0),
